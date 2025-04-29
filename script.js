d9f33095db67cee7d985a74767f072ca2f4a6599
Arthur Roberto Alves <arthurrobertoalves@gmail.com>
2025-04-28 23:14:19 -0300

    create sintaxe in js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function tabuada(){
-    
+function tabuada() {
+    let numero = Number(document.getElementById('number').value);
+    let i = 1;
+    let resultado = '';
+    while (i <= 10) {
+        resultado += numero + ' * ' + i + ' = ' + (numero * i) + '<br>';
+        i++;
+    }
+    document.getElementById("text").innerHTML = resultado;
 }
